a7f7e25dc1ae033562cbc17ee0361375c02c9512
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-12-07 16:48:35 +0300

    task the som of range

---
+++ src/scripts/index.js
@@ -0,0 +1,26 @@
+//task the som of range
+/*function range(start, end, step = start < end ? 1 : -1) {
+    let arr = [];
+
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) {
+            arr.push(i);
+        }
+    } else {
+        for (let i = end; i >= start; i += step) {
+            arr.push(i);
+        }
+    }
+
+    return arr
+}
+
+range(2, 10, 3);
+
+
+function sum(arr) {
+    let sum = 0;
+    for (let i of arr) {
+        sum += i;
+    }
+}*/
