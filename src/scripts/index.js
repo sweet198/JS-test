34d119d7ff9b81067c23dd86a92479704a46aa9f
Vladislav Svitkovskiy <svitkovskiy198@gmail.com>
2020-12-14 19:04:27 +0300

    started test of yandex

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-//task the som of range
+//task the sum of range
 /*function range(start, end, step = start < end ? 1 : -1) {
     let arr = [];
 
@@ -24,8 +24,8 @@ function sum(arr) {
         sum += i;
     }
 }*/
-
-let arr = [1, 2, 3, 4, 5, 6];
+//second task
+/*let arr = [1, 2, 3, 4, 5, 6];
 
 function reverseArray(arr) {
     let newArr = [];
@@ -45,4 +45,57 @@ function reverseArrayInPlace(arr) {
     return arr;
 }
 
-reverseArrayInPlace(arr);
+reverseArrayInPlace(arr);*/
+
+//third task
+/*
+let arr = [1, 2, 3];
+
+const arrayToList = (array) => {
+    let list = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+        list = {value: array[i], rest: list};
+    }
+
+    return list;
+}
+
+arrayToList(arr);
+
+const listToArray = (list) => {
+    let array = [];
+
+    for (let node = list; node; node = node.rest) {
+       array.push(node.value);
+    }
+    console.log(array);
+}
+
+listToArray(arrayToList(arr));
+
+const prepend = (elem, list) => {
+    let newList = {};
+    newList.value = elem;
+    newList.rest = list;
+
+    return newList;
+}
+
+console.log(prepend(2, arrayToList(arr)));*/
+
+//test for yandex
+
+const makeBoard = (width, height) => {
+    let board = '';
+    for (let i = 0; i < height; i++) {
+        for (let k = 0; k < width; k++) {
+            board += generateRandomCell();
+        }
+        board += "\n";
+    }
+    console.log(board);
+}
+
+const generateRandomCell = () => Math.round(Math.random());
+
+makeBoard(6, 3);
